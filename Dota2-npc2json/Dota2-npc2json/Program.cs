9acae857f4a39c4427d586b83f2f65dd7a0cce49
Joshua McMorran <mcmorranjoshua@yahoo.co.uk>
2019-10-26 14:54:38 +0100

    Added newtonsoft.json and set project up

---
+++ Dota2-npc2json/Dota2-npc2json/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2_npc2json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
